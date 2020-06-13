43c0cfe6fdef7c1e387962a51c46673011aacf1c
janosoto <alejandro.soto@usach.cl>
2020-06-13 19:43:02 -0400

    ADD Opción para descargar las notas

--- src/FinishedMeeting.js
+++ src/FinishedMeeting.js
@@ -4,7 +4,45 @@ import MeetingResume from './MeetingResume';
 import Note from './Note';
 import Chronometer from './Chronometer';
 
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+
 class FinishedMeeting extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.exportToXLSX = this.exportToXLSX.bind(this);
+  }
+  s2ab = (s) => { 
+    var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+    var view = new Uint8Array(buf);  //create uint8array as viewer
+    for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+    return buf;    
+  }
+
+  exportToXLSX = () => {
+    const data = this.props.notes.map((note, i) => {
+      return {
+        number:i+1, 
+        summary:note.summary, 
+        category:note.category, 
+        responsable:note.responsable
+      }
+    });
+    const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    const fileExtension = '.xlsx';
+    const headers = [['#', 'Resumen', 'Tipo', 'Responsable']]
+    const ws = XLSX.utils.aoa_to_sheet(headers);
+    XLSX.utils.sheet_add_json(ws, data, {
+      header:['number', 'summary', 'category', 'responsable'],
+      skipHeader:true,
+      origin:-1
+    });
+    const wb = { Sheets: { 'Notas': ws }, SheetNames: ['Notas'] };
+    const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    const file = new Blob([excelBuffer], {type: fileType});
+    FileSaver.saveAs(file, this.props.name + fileExtension);
+  }
 
   componentDidMount() {
     document.querySelector('.fixed-header').classList.add('hidden');
@@ -32,7 +70,9 @@ class FinishedMeeting extends React.Component {
         <div className="row">
           <div className="col-9">
             <div className="component-container">
-              <h2>Reunión terminada</h2>
+              <h2>
+                Reunión terminada
+              </h2>
               <MeetingData 
                 name={this.props.name} 
                 target={this.props.target} 
@@ -57,9 +97,17 @@ class FinishedMeeting extends React.Component {
             </div>
           </div>
         </div>
-        <div>
+        <div className="notes-container container">
           <div className="component-container">
-            <h2>Notas tomadas</h2>
+            <h2>
+              Notas tomadas
+              <button 
+                className="btn btn-primary"
+                onClick={this.exportToXLSX}
+              >
+                <i className="fas fa-download"></i> Descargar notas
+              </button>
+            </h2>
             <div className="row header">
               <div className="col-1">#</div>
               <div className="col-5">Resumen</div>
