cd9db09c6f33e2d3f2e946dc1c77f789f2e4514c
janosoto <alejandro.soto@usach.cl>
2020-06-13 20:06:01 -0400

    Updates

---
+++ precache-manifest.169f89f29e92bd683821678273cfecfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553fab4522d44aa42144dbdb35f6e494",
+    "url": "/meeting-notes/index.html"
+  },
+  {
+    "revision": "ee9561213245c7747364",
+    "url": "/meeting-notes/static/css/2.5e4ff3cb.chunk.css"
+  },
+  {
+    "revision": "da4fa4b434abc3ebb210",
+    "url": "/meeting-notes/static/css/main.d9728e14.chunk.css"
+  },
+  {
+    "revision": "ee9561213245c7747364",
+    "url": "/meeting-notes/static/js/2.06079ec6.chunk.js"
+  },
+  {
+    "revision": "38ed67aeb591a4e3d95794f1194f61a5",
+    "url": "/meeting-notes/static/js/2.06079ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da4fa4b434abc3ebb210",
+    "url": "/meeting-notes/static/js/main.2fd8d499.chunk.js"
+  },
+  {
+    "revision": "05b7dca4e12d780dc71e",
+    "url": "/meeting-notes/static/js/runtime-main.0d7ee8ba.js"
+  }
+]);
